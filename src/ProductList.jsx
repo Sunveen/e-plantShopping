bae6558e237b6b1487edd98ff12f7aed74373b08
Sunveen <sunveen07@gmail.com>
2026-03-09 13:18:54 -0400

    added item add and remove functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch, useSelector } from 'react-redux';
+import {addItem,removeItem} from './CartSlice'
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const dispatch= useDispatch()
+    const cartItems = useSelector(state=> state.cart.items)
 
     const plantsArray = [
         {
@@ -248,10 +252,22 @@ function ProductList({ onHomeClick }) {
         setShowCart(false); // Hide the cart when navigating to About Us
     };
 
+    const addPlant= (plant)=> {
+        dispatch(addItem(plant))
+    }
+
+    const removePlant= (plant)=> {
+        console.log({name:plant.name},"ProductList");
+        dispatch(removeItem(plant.name))
+    }
+
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const totalCartItems= cartItems.reduce((total, item)=> total+item.quantity,0)
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -269,14 +285,56 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>{totalCartItems} <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                {plantsArray.map((plantGroup, index) => (
+                    <React.Fragment key={index}>
 
+                    <h1 className="product-list">{plantGroup.category}</h1>
 
-                </div>
+                    <div className="product-list">
+                        {plantGroup.plants.map((plant, i) => (
+                        <div className="product-card" key={i}>
+
+                            <img
+                            src={plant.image}
+                            alt={plant.name}
+                            className="product-image"
+                            />
+
+                            <div className="product-title">{plant.name}</div>
+
+                            <p className="product-description">
+                            {plant.description}
+                            </p>
+
+                            <div className="product-cost">
+                            {plant.cost}
+                            </div>
+                            <button
+                                className="product-button"
+                                disabled={cartItems.some(item => item.name === plant.name)}
+                                onClick={() => addPlant(plant)}
+                                >
+                                Add
+                            </button>
+
+                            <button
+                                className="product-button"
+                                onClick={() => removePlant(plant)}
+                                >
+                                Remove
+                            </button>
+                        </div>
+                        ))}
+                    </div>
+
+                    </React.Fragment>
+                ))}
+</div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
             )}
